36791a90353c4aeed50abddf9c9789672e16ce90
Deggen <d.kellenschwiler@bsvassociation.org>
2025-09-12 10:57:23 -0500

    Bump version to 0.6.4

--- src/main.tsx
+++ src/main.tsx
@@ -32,7 +32,7 @@ if (rootElement) {
             onWalletReady={onWalletReady}
             nativeHandlers={tauriFunctions}
             appVersion={packageJson.version}
-            appName="Metanet Desktop"
+            appName="BSV Desktop"
           />
         </ErrorBoundary>
         <ToastContainer
